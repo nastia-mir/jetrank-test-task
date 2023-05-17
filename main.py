533c45b430f0c58436053d98eacfd07fb6e2f381
Anastasia Mir <nastasia.ua@gmail.com>
2023-05-17 16:08:42 +0300

    saving results to file + fixed issue with Cyrillics

--- main.py
+++ main.py
@@ -13,38 +13,36 @@ def get_results(search_term, num_top_results):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     result_divs = soup.find_all("div", attrs={"class": "Gx5Zad fP1Qef xpd EtOod pkphOe"})
-    result_data = {"Data": []}
     position = 0
-    for div in result_divs:
-        # print(div.prettify())
-        website_info = {}
-        position += 1
-        website_info['position'] = position
-        try:
-            title = div.find("h3").getText()
-            website_info['title'] = title
-        except:
-            website_info['title'] = ""
+    with open('result.json', 'w', encoding='utf-8') as file:
+        for div in result_divs:
+            # print(div.prettify())
+            website_info = {}
+            position += 1
+            website_info['position'] = position
+            try:
+                title = div.find("h3").getText()
+                website_info['title'] = title
+            except:
+                website_info['title'] = ""
 
-        try:
-            result_url = div.find("a", href=True)['href']
-            website_info['url'] = result_url.split('&')[0][7:]
-        except:
-            website_info['url'] = ""
+            try:
+                result_url = div.find("a", href=True)['href']
+                website_info['url'] = result_url.split('&')[0][7:]
+            except:
+                website_info['url'] = ""
 
-        try:
-            description = div.find("div", attrs={"class": "BNeawe s3v9rd AP7Wnd"}).getText()
-            website_info['description'] = description
-        except:
-            website_info['description'] = ""
+            try:
+                description = div.find("div", attrs={"class": "BNeawe s3v9rd AP7Wnd"}).getText()
+                website_info['description'] = description
+            except:
+                website_info['description'] = ""
 
-        result_data['Data'].append(website_info)
-
-    return result_data
+            json.dump(website_info, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     search_term = input("Enter search term: ")
     num_top_results = int(input("Enter number of results: "))
-    results = get_results(search_term, num_top_results)
-    print(json.dumps(results, indent=4))
+    get_results(search_term, num_top_results)
+
